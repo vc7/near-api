e3e1e6b6a3202ef56ad2a9355afcac6198c7a183
vc7 <vince78718@gmail.com>
2017-02-04 17:28:29 +0900

    Format the output of spot service's `find_trend_spots` method.

--- app/services/spot_service.rb
+++ app/services/spot_service.rb
@@ -16,12 +16,38 @@ class SpotService
         { 
           :client_id => client_id,
           :client_secret => client_secret,
-          :v => '20170202',
+          :v => Date.today.strftime('%Y%m%d'),
           :limit => 10,
           :ll => "#{latitude},#{longitude}", 
           :locale => 'ja'
         } 
       }
     response = self.get("/", parameters).parsed_response
+
+    if response['meta']['code'] != 200 
+      return []
+    end
+
+    spots = []
+    raw_spots = response['response']['venues']
+
+    raw_spots.each do |raw_spot|
+      spot = {
+        :id => raw_spot['id'],
+        :name => raw_spot['name'],
+        :location => {
+          :latitude => raw_spot['location']['lat'],
+          :longitude => raw_spot['location']['lng'],
+          :distance => raw_spot['location']['distance'],
+          :address => raw_spot['location']['formattedAddress'].reverse.join(" ")
+        },
+        :category_icon => "#{raw_spot['categories'][0]['icon']['prefix']}88#{raw_spot['categories'][0]['icon']['suffix']}",
+        :here_count => raw_spot['hereNow']['count']
+      }
+
+      spots.push(spot)
+    end
+
+    return spots
   end
 end
